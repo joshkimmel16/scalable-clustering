e03332ade02fa99217b2a46b932d97f57c8fccec
Bobby Judd <bobbymjudd@gmail.com>
2020-05-27 16:47:07 -0700

    Initial dataparser implementation

--- include/dataparser.h
+++ include/dataparser.h
@@ -1,5 +1,24 @@
 #ifndef DATAPARSER_H
 #define DATAPARSER_H
 
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stdexcept> // std::runtime_error
+#include <sstream> // std::stringstream
+#include "datapoint.h"
+#include "config.h"
+#include "clustergraph.h"
 
+class DataParser {
+public:
+    DataParser(Config dataConfig); //constructor 
+    ~DataParser(); //destructor
+    void LoadInputData();
+
+private:
+    Config dataConfig; // Config of input dataset to load
+    unsigned int numAttributes; // Number of columns per data point retrieved from Config
+    std::vector<std::string> attrNames; // List of column headers
+};
 #endif
--- src/dataparser.cc
+++ src/dataparser.cc
@@ -1,1 +1,58 @@
 #include "dataparser.h"
+
+DataParser::DataParser(Config dataConfig) {
+    dataConfig = dataConfig;
+}
+
+DataParser::~DataParser() {
+
+}
+
+void DataParser::LoadInputData() {
+    
+    // Open CSV file at filePath
+    std::ifstream inputDataFile(dataConfig.GetDataPath());
+
+    unsigned int numAttrs = dataConfig.GetNumAttrs();
+    attrNames = std::vector<std::string>();
+
+    // Make sure the file is open
+    if(!inputDataFile.is_open()) throw std::runtime_error("Could not open file");
+
+    std::string line, colname, dataField;
+    int val;
+
+    // Read the column headers
+    if(inputDataFile.good())
+    {
+        std::getline(inputDataFile, line);
+        std::stringstream ss(line);
+
+        // Extract each column name
+        while(std::getline(ss, colname, ','))
+            attrNames.push_back(colname);
+    }
+
+    // Read data, line by line
+    DataPoint* currentDp;
+    std::string csvRow[numAttrs];
+    int colIdx;
+    while(std::getline(inputDataFile, line))
+    {
+        std::stringstream ss(line);
+        colIdx = 0;
+        currentDp = new DataPoint(numAttrs);
+        while(std::getline(ss, dataField, ',')) {
+            csvRow[colIdx++] = dataField;
+        }
+
+        // Poplate the DataPoint's raw data
+        currentDp->PopulateRawData(csvRow);
+
+        //TODO: Feed to Cluster to be processed
+    }
+
+    // Close file
+    inputDataFile.close();
+
+}
