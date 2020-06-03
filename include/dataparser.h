6698e22032e9d0a563b88c226d5f5a28b94a30d8
Bobby Judd <bobbymjudd@gmail.com>
2020-06-02 18:04:06 -0700

    Change to batched data loading

--- include/dataparser.h
+++ include/dataparser.h
@@ -4,21 +4,30 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include <stdexcept> // std::runtime_error
-#include <sstream> // std::stringstream
+#include <stdexcept>
+#include <sstream>
 #include "datapoint.h"
 #include "config.h"
 #include "clustergraph.h"
 
 class DataParser {
 public:
-    DataParser(Config dataConfig); //constructor 
+    DataParser(Config dataConfig, unsigned int batchSize); //constructor 
     ~DataParser(); //destructor
-    void LoadInputData();
+    std::vector<std::string> GetAttrNames(); // Returns column header vector
+    void LoadHeaders(); // Reads column headers into attrNames
+    unsigned int LoadNextDataBatch(); // Loads next bSize lines into rawData
+    std::vector<std::vector<std::string>> GetRawData();
 
 private:
-    Config dataConfig; // Config of input dataset to load
+    Config dConf; // Config of input dataset to load
+    std::ifstream inputDataFile;
+    bool readComplete; // true after the entire csv file has been read, else false
     unsigned int numAttributes; // Number of columns per data point retrieved from Config
+    unsigned int bSize; // Internal batch size
+    unsigned int lastLine; // Memory of last read line for batch continuation
     std::vector<std::string> attrNames; // List of column headers
+    std::vector<std::vector<std::string>> rawData;
+
 };
 #endif
