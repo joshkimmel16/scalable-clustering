b155c2a954c70b02a4964f6a24b5baa20e0279f9
Josh Kimmel <joshkimmel16@g.ucla.edu>
2020-06-13 10:40:48 -0700

    Adding support for yyyy-mm-dd date format. Updating date type to use this format by default.

--- tests/compare_test.cc
+++ tests/compare_test.cc
@@ -27,6 +27,9 @@ TEST_F(CompareTest, Date) {
   std::string t2 = "1/1/2020";
   std::string t3 = "12/31/1980";
 
+  std::string t4 = "1992-06-14";
+  std::string t5 = "1992-08-01";
+
   Date d1 = Date(t1, MONTH_DAY_YEAR);
   EXPECT_EQ(d1.month, 7);
   EXPECT_EQ(d1.year, 1992);
@@ -37,20 +40,25 @@ TEST_F(CompareTest, Date) {
   EXPECT_EQ(d1.compare(d2), true);
   EXPECT_EQ(d1.compare(d3), false);
   EXPECT_EQ(d1.compare(d1), true);
+
+  Date d4 = Date(t4, YEAR_MONTH_DAY);
+  Date d5 = Date(t5, YEAR_MONTH_DAY);
+  EXPECT_EQ(d1.compare(d4), false);
+  EXPECT_EQ(d1.compare(d5), true);
 }
 
 //Compare test
 TEST_F(CompareTest, Compare) {
-    std::string anch_data[4] = { "j", "3", "1.555", "7/14/1992" };
+    std::string anch_data[4] = { "j", "3", "1.555", "1992-07-14" };
     unsigned int anch_len = 4;
     DataPoint* anch = new DataPoint(anch_len);
     anch->PopulateRawData(anch_data);
 
-    std::string raw_1[4] = { "a", "1", "1.54", "6/20/1991" };
+    std::string raw_1[4] = { "a", "1", "1.54", "1991-06-20" };
     unsigned int raw_1_len = 4;
-    std::string raw_2[4] = { "j", "3", "1.555", "7/14/1992" };
+    std::string raw_2[4] = { "j", "3", "1.555", "1992-07-14" };
     unsigned int raw_2_len = 4;
-    std::string raw_3[4] = { "p", "10", "1.7", "4/20/2000" };
+    std::string raw_3[4] = { "p", "10", "1.7", "2000-04-20" };
     unsigned int raw_3_len = 4;
     DataPoint* d1 = new DataPoint(raw_1_len);
     d1->PopulateRawData(raw_1);
